35220080de63f109229f421bd90bbbcb04789523
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-15 11:20:08 +0530

    Add Fibonacci series program

---
+++ Loops/FibonacciSeries.py
@@ -0,0 +1,14 @@
+# Fibonacci Series
+
+n = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
