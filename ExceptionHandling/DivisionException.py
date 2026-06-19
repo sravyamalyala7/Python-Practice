2126cbbeb91033d13b8fc4d30aea8416c392ee72
sravyamalyala7 <sravyamalyala7@gmail.com>
2026-06-19 12:58:10 +0530

    Add division exception handling program

---
+++ ExceptionHandling/DivisionException.py
@@ -0,0 +1,13 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    result = num1 / num2
+
+    print("Result =", result)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+except ValueError:
+    print("Please enter valid numbers.")
